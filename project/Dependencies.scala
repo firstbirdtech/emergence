c5c075ace856df1029385c465222f3731155c040
Fabian Grutsch <f.grutsch@gmail.com>
2021-10-31 02:25:27 +0200

    Update fs2-core, fs2-io to 3.2.2 (#37)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   private val circeVersion      = "0.14.1"
-  private val fs2Version        = "3.1.6"
+  private val fs2Version        = "3.2.2"
   private val sttpClientVersion = "3.3.15"
 
   val core: Seq[ModuleID] = Seq(
