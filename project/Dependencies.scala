4b2fa530fc81c0343d4d4eda168ff7996712433c
Fabian Grutsch <f.grutsch@gmail.com>
2022-03-13 01:33:12 +0100

    Update jackson-dataformat-yaml to 2.13.2 (#102)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     "ch.qos.logback"                   % "logback-classic"                % "1.2.11",
     "co.fs2"                          %% "fs2-io"                         % fs2Version,
     "co.fs2"                          %% "fs2-core"                       % fs2Version,
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml"        % "2.13.1",
+    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml"        % "2.13.2",
     "com.monovore"                    %% "decline-effect"                 % "2.2.0",
     "com.softwaremill.sttp.client3"   %% "core"                           % sttpClientVersion,
     "com.softwaremill.sttp.client3"   %% "circe"                          % sttpClientVersion,
