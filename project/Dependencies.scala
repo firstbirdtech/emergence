b63c3ebbc97d7d346ebcb20e908a692b3891bd0d
Fabian Grutsch <f.grutsch@gmail.com>
2022-07-17 02:45:16 +0200

    Update client3:async-http-client-backend-cats, ... to 3.7.0 (#138)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   private val circeVersion      = "0.14.1"
   private val fs2Version        = "3.2.10"
-  private val sttpClientVersion = "3.6.2"
+  private val sttpClientVersion = "3.7.0"
 
   val core: Seq[ModuleID] = Seq(
     "ch.qos.logback"                   % "logback-classic"                % "1.2.11",
