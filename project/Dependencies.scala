293eefcf5aaa036671b098d17ecac2bdbc94c33e
Fabian Grutsch <f.grutsch@gmail.com>
2022-08-07 02:49:10 +0200

    Update fs2-core, fs2-io to 3.2.11 (#143)
    * Update fs2-core, fs2-io to 3.2.11
    
    * Revert commit(s) 1a2d483
    
    * Update fs2-core, fs2-io to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   private val circeVersion      = "0.14.1"
-  private val fs2Version        = "3.2.10"
+  private val fs2Version        = "3.2.11"
   private val sttpClientVersion = "3.7.2"
 
   val core: Seq[ModuleID] = Seq(
