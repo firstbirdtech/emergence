a4619de78674dbf2977c593069fe39587c10ade0
Fabian Grutsch <f.grutsch@gmail.com>
2022-10-30 03:28:05 +0100

    Update circe-yaml to 0.14.2 (#181)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     "com.typesafe"                     % "config"                         % "1.4.2",
     "io.circe"                        %% "circe-core"                     % circeVersion,
     "io.circe"                        %% "circe-parser"                   % circeVersion,
-    "io.circe"                        %% "circe-yaml"                     % "0.14.1",
+    "io.circe"                        %% "circe-yaml"                     % "0.14.2",
     "org.scalatest"                   %% "scalatest"                      % "3.2.14" % Test,
     "org.typelevel"                   %% "cats-effect"                    % "3.3.14",
     "org.typelevel"                   %% "log4cats-slf4j"                 % "2.5.0"
