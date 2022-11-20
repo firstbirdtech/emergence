2ca6f7f671c74603ee4e2f6a7edf9d12e2091802
Fabian Grutsch <f.grutsch@gmail.com>
2022-11-20 03:01:13 +0100

    Update logback-classic to 1.4.5 (#190)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val sttpClientVersion = "3.8.2"
 
   val core: Seq[ModuleID] = Seq(
-    "ch.qos.logback"                   % "logback-classic"                % "1.4.4",
+    "ch.qos.logback"                   % "logback-classic"                % "1.4.5",
     "co.fs2"                          %% "fs2-io"                         % fs2Version,
     "co.fs2"                          %% "fs2-core"                       % fs2Version,
     "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml"        % "2.14.0",
