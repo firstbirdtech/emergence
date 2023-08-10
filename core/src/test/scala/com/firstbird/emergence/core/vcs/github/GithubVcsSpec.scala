6342377538e095e76348d4da0b89738a953c4392
Benjamin Maurer <benjamin.maurer@firstbird.com>
2023-08-10 15:47:50 +0200

    Delete branch after merge on Github.

--- core/src/test/scala/com/firstbird/emergence/core/vcs/github/GithubVcsSpec.scala
+++ core/src/test/scala/com/firstbird/emergence/core/vcs/github/GithubVcsSpec.scala
@@ -127,6 +127,11 @@ class GithubVcsSpec extends BaseSpec {
         """).value.toString
     )
     .whenRequestMatches { r =>
+      r.uri.path.startsWith("repos" :: "owner" :: "name" :: "git" :: "refs" :: "heads" :: "update/abc" :: Nil) &&
+      r.method == Method.DELETE
+    }
+    .thenRespondWithCode(StatusCode.NoContent)
+    .whenRequestMatches { r =>
       r.uri.path.startsWith("repos" :: "owner" :: "name" :: "contents" :: ".emergence.yml" :: Nil) &&
       r.method == Method.GET &&
       r.headers.contains(Header("Accept", "application/vnd.github.raw"))
@@ -143,6 +148,7 @@ class GithubVcsSpec extends BaseSpec {
     BranchName("master"),
     Author("fgrutsch")
   )
+
   test("listPullRequests") {
     val result = githubVcs.listPullRequests(Repository("owner", "name")).unsafeRunSync()
     result mustBe {
